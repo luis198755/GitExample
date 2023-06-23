2ec3c16d3658e619463009ef016a2d38f5d54c05
lrodriguez <lrodriguez@trafficlight.mx>
2023-06-23 10:21:32 -0600

    fractal

--- prueba.py
+++ prueba.py
@@ -1,1 +1,37 @@
-print("hello")
+import turtle
+
+def draw_fractal_tree(branch_length, angle, scale_factor, num_branches, depth):
+    if depth == 0:  # Base case: stop recursion when depth reaches zero
+        return
+    else:
+        turtle.forward(branch_length)  # Draw the trunk
+
+        for _ in range(num_branches):
+            turtle.left(angle)  # Turn left to draw the left subtree
+            draw_fractal_tree(branch_length * scale_factor, angle, scale_factor, num_branches, depth - 1)
+
+        turtle.right(angle * num_branches * 2)  # Turn right to draw the right subtree
+        draw_fractal_tree(branch_length * scale_factor, angle, scale_factor, num_branches, depth - 1)
+
+        turtle.left(angle * num_branches)  # Turn back to the original direction
+        turtle.backward(branch_length)  # Go back to the trunk's starting point
+
+# Setup turtle
+turtle.setup(width=800, height=600)
+turtle.speed(0)  # Set the turtle's speed (0 is the fastest)
+turtle.bgcolor("black")  # Set the background color
+turtle.pencolor("white")  # Set the pen color
+
+# Position turtle at the bottom of the screen
+turtle.penup()
+turtle.goto(0, -250)
+turtle.pendown()
+
+# Start drawing the fractal tree
+draw_fractal_tree(50, 30, 1.0, 2, 5)  # Adjust the parameters to create different types of fractals
+
+# Hide turtle
+turtle.hideturtle()
+
+# Keep the window open until it's manually closed
+turtle.done()
